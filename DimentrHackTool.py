b2910558bdbe9b19cb7f6357837e6ce27a2fb974
DimentrRec <61883243+DimentrRec@users.noreply.github.com>
2020-03-25 17:02:25 +0400

    Update DimentrHackTool.py

--- DimentrHackTool.py
+++ DimentrHackTool.py
@@ -14,7 +14,11 @@ print("2. DDos-Attack")
 print("3. MetaSploit(Не Работает)")
 print("4. QiwiSoft")
 print("5. Impulse")
-print("6. instashell")
+print("6. Instashell")
+print("7. Twitshell") 
+print("8. Sqlmap")
+print("9. IPGeoLocation")
+print("10. Generation-Password")
 print("")
 
 inp = input(">> ")
@@ -38,3 +42,15 @@ if inp =="5":
 if inp == "6":
    subprocess.call("pkg install tor curl openssl*", shell=True)
    subprocess.call("git clone https://github.com/thelinuxchoice/instashell", shell=True)
+  
+ if inp =="7":
+   subprocess.call("git clone https://github.com/thelinuxchoice/tweetshell", shell=True)
+
+if inp =="8":
+  subprocess.call("pkg install sqlmap", shell=True)
+ 
+if inp =="9":
+  subprocess.call("git clone https://github.com/maldevel/IPGeoLocation", shell=True)
+ 
+if inp =="10":
+  subprocess.call("git clone https://github.com/DimentrRec/Generation-Password", shell=True)thelinuxchoice/instashell", shell=True)
